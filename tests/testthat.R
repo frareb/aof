e271c9a46c73bcc6501468e6f0965050841408e8
Francois Rebaudo <francois.rebaudo@ird.fr>
2020-01-31 17:43:24 +0100

    coverage

---
+++ tests/testthat.R
@@ -0,0 +1,10 @@
+library(testthat)
+library(osfi)
+
+test_check("osfi")
+
+# devtools::test()
+# covr::package_coverage()
+# covr::report()
+# devtools::document()
+# devtools::check()
---
+++ tests/testthat/test_osfi.R
@@ -0,0 +1,31 @@
+
+test_that("osfi return data.frame",{
+  mu1 <- 50
+  mu2 <- 50
+  rho1 = 0.5
+  rho2 = 0.5
+  n.obs <- 5
+  sigma1 <- 0.1
+  sigma2 <- sigma1
+  SimTS <- function(n, mu, rho, sigma){
+    X.standard <- arima.sim(n, model = list(ar = rho))
+    X.standard/sd(X.standard)*sigma + mu
+  }
+  t.full <- 0:50
+  t.break <- 25
+  x.full <- c(SimTS(t.break, mu1, rho1, sigma1),
+    SimTS(max(t.full)-t.break+1, mu2, rho2, sigma2))
+  keep <- sort(sample(1:length(x.full), n.obs))
+  TimeBudget <- data.frame(
+    bee = "A",
+    Age = t.full[keep],
+    x = x.full[keep])
+  AOF <- osfi(
+    bee = TimeBudget$bee,
+    Age = TimeBudget$Age,
+    x = TimeBudget$x)
+  expect_equal(
+    object = class(AOF),
+    expected = "data.frame"
+  )
+})
